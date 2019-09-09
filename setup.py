395a6d99fef988d472e5d518bf0db34406f5546d
Malte Metz <mmetz@gfz-potsdam.de>
2019-09-09 09:54:35 +0200

    add okada modelling and pseudo-dynamic rupture model (chapter 1)
    - implementation of basic functionality

--- setup.py
+++ setup.py
@@ -523,6 +523,7 @@ subpacknames = [
     'pyrocko.apps',
     'pyrocko.io',
     'pyrocko.model',
+    'pyrocko.modelling',
     'pyrocko.plot',
     'pyrocko.gui',
     'pyrocko.gui.snufflings',
@@ -689,6 +690,13 @@ setup(
             include_dirs=[get_python_inc()],
             extra_compile_args=['-Wno-parentheses', '-Wno-uninitialized'],
             extra_link_args=[] if sys.platform != 'sunos5' else ['-Wl,-x']),
+
+        Extension(
+            'modelling.okada_ext',
+            include_dirs=[get_python_inc(), numpy.get_include()],
+            extra_compile_args=['-Wextra'] + omp_arg,
+            extra_link_args=[] + omp_lib,
+            sources=[op.join('src', 'modelling', 'ext', 'okada_ext.c')])
     ],
 
     scripts=[
