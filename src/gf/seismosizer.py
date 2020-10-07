130821843c932c2f8b14555a49cdc6c0f718589a
mmetz <mmetz@gfz-potsdam.de>
2020-10-07 14:10:39 +0200

    add okada modelling and pseudo-dynamic rupture model (chapter 16)
    - bugfixes
    - polishing (patch2m6 parallisation)

--- src/gf/seismosizer.py
+++ src/gf/seismosizer.py
@@ -3115,6 +3115,7 @@ class PseudoDynamicRupture(SourceWithDerivedMagnitude):
             disl_norm=slip_norm,
             lamb=lamb,
             mu=mu)
+            # nthreads=self.nthreads)
 
         m6s *= patch_area
 
