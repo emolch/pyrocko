1c573439ac5ccc033c529406c55cacbfe892ba16
Sebastian Heimann <sebastian.heimann@gfz-potsdam.de>
2021-04-08 14:40:25 +0200

    windows support: wip

--- prerequisites/prerequisites.bat
+++ prerequisites/prerequisites.bat
@@ -1,6 +1,10 @@
 
 :: "c:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-tar -xzf libmseed-2.19.6.tar.gz
-cd libmseed
-nmake -f Makefile.win lib
+IF NOT EXIST "libmseed\libmseed.lib" (
+    tar -xzf libmseed-2.19.6.tar.gz
+    cd libmseed
+    nmake -f Makefile.win lib
+) ELSE (
+    ECHO libmseed found
+)
